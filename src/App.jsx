a7f732d38f2c3e56e064bbc1e46eb683e729c2ac
Du Han <handuwudi@outlook.com>
2025-03-23 18:30:26 -0400

    Change input types for website and LinkedIn fields to URL in CVGenerator

--- src/App.jsx
+++ src/App.jsx
@@ -445,12 +445,12 @@ function CVGenerator() {
 
         <div style={{ display: "flex", alignItems: "center", gap: "10px" }}>
             <label style={{ width: "50px" }}>Website:</label>
-            <input type="tel" ref={websiteRef} style={{ padding: "5px", width: "200px" }}/>
+            <input type="url" ref={websiteRef} style={{ padding: "5px", width: "200px" }}/>
         </div>
 
         <div style={{ display: "flex", alignItems: "center", gap: "10px" }}>
             <label style={{ width: "50px" }}>Linkedin:</label>
-            <input type="tel" ref={linkedinRef} style={{ padding: "5px", width: "200px" }}/>
+            <input type="url" ref={linkedinRef} style={{ padding: "5px", width: "200px" }}/>
         </div>
       </div>
       <div><button onClick={addWorkExp}>Add Work Experience</button></div>
